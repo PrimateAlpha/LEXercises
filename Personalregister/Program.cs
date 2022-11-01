890246a4b2518095aea5288eda31e5b8dd5b85cf
Christian Norman <christian.norman@hotmail.se>
2022-11-01 16:00:21 +0100

    v1.

---
+++ Personalregister/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Personalregister
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Personalregister");
+
+
+            Console.Write("Medarbetarens förnamn? ");
+            string firstName = Console.ReadLine();
+
+            Console.Write("Medarbetarens Efternamn? ");
+            string lastName = Console.ReadLine();
+
+            Console.Write("Employers salary? ");
+            string salary = Console.ReadLine();
+
+
+            DisplayResult(firstName, lastName, salary);
+
+
+            Console.ReadLine();
+
+        }
+
+        private static string ReverseString(string message)
+        {
+            char[] messageArray = message.ToCharArray();
+            Array.Reverse(messageArray);
+            return String.Concat(messageArray);
+        }
+
+        private static void DisplayResult(string firstName, string lastName, string salary)
+        {
+            Console.WriteLine("Medarbetarens uppgifter: ");
+            Console.Write(String.Format("{0} {1} {2}",
+                firstName,
+                lastName + ",",
+                "Lön: " + salary + " SEK/månad"));
+        }
+
+        private static void DisplayResult(string message)
+        {
+            Console.WriteLine("Results: ");
+            Console.Write(message);
+
+        }
+    }
+}
